457278bd9d61ef1d792629045a21c6712126cbe9
Casper <casper.bjorkman@gmail.com>
2022-10-04 12:52:50 +0200

    added token class

--- parsing/scanner.py
+++ parsing/scanner.py
@@ -1,4 +1,23 @@
+from dataclasses import dataclass
+from enum import Enum, auto
 from typing import Iterable
+
+
+class TokenKind(Enum):
+    plus = auto
+    minus = auto
+    multiply = auto
+    divide = auto
+
+    open_paren = auto
+    closed_paren = auto
+
+
+@dataclass
+class Token:
+    lexeme: str
+    index: int
+    kind: TokenKind
 
 
 def illigal_chars(text: str) -> Iterable[int]:
