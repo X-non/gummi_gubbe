b80aadb583ca8bc71135f052071d4653746592a1
Casper <casper.bjorkman@gmail.com>
2022-10-13 10:24:42 +0200

    small formating and latex fixes

--- pp/PP.py
+++ pp/PP.py
@@ -1,30 +1,33 @@
 test = {
-"kind": "add", 
+    "kind": "add",
     "left": {
-    "kind": "add", 
-        "left": {"kind": "num", "val": "3"}, 
-        "right":{ 
-        "kind": "div",
-        "left": {"kind": "num", "val": "3"}, 
-        "right":{"kind": "num", "val": "4"}
-        }} , 
-    "right": {"kind": "num", "val": "36"}
+        "kind": "add",
+        "left": {"kind": "num", "val": "3"},
+        "right": {
+            "kind": "div",
+            "left": {"kind": "num", "val": "3"},
+            "right": {"kind": "num", "val": "4"},
+        },
+    },
+    "right": {"kind": "num", "val": "36"},
 }
 
 Symbols = {
-    "add" : "+",
-    "sub" : "-",
-    "mul" : "\cdot",
-    "div" : "\\fract",
-    "par" : "()"
+    "add": "+",
+    "sub": "-",
+    "mul": "\\cdot",
+    "div": "\\frac",
+    "par": "()",
 }
+
 
 def read_symb(node_tree):
     if node_tree["kind"] in Symbols:
-            return Symbols[node_tree["kind"]]
+        return Symbols[node_tree["kind"]]
     else:
         return "err"
-           
+
+
 def read_val(node_tree, direction):
     if node_tree[direction]["kind"] == "num":
         return node_tree[direction]["val"]
@@ -35,17 +38,19 @@ def read_val(node_tree, direction):
         if read_symb(a) != "\\fract":
             return f"{left_str} {read_symb(a)} {right_str}"
         else:
-            return (f"{read_symb(a)}{{{left_str}}}{{{right_str}}}")
- 
+            return f"{read_symb(a)}{{{left_str}}}{{{right_str}}}"
+
+
 def temp_list(node_tree, direction):
-        temp_list = node_tree[direction]
-        return (temp_list)
+    temp_list = node_tree[direction]
+    return temp_list
+
 
 def add(node_tree):
     if node_tree["kind"] == "add":
         left = read_val(node_tree, "left")
         right = read_val(node_tree, "right")
-        return f"{left} + {right}"  
+        return f"{left} + {right}"
+
 
 print(add(test))
-
