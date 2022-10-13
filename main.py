e577e77adab66dc3ce48ebbdc83ea2ec6d5b63df
Casper <casper.bjorkman@gmail.com>
2022-10-13 10:59:20 +0200

    basic demo start

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from pprint import pprint
 from unittest import result
-
+import demo
 from parsing.parser import parse
 from parsing.scanner import scanner
 
-while True:
-    text = input()
-    for a in scanner(text):
-        print(a)
-    try:
-        result = parse(text)
-        pprint(result, sort_dicts=True)
+# while True:
+#     text = input()
+#     for a in scanner(text):
+#         print(a)
+#     try:
+#         result = parse(text)
+#         pprint(result, sort_dicts=True)
 
-    except Exception as e:
-        print("Err:", e)
+#     except Exception as e:
+#         print("Err:", e)
+demo.start()
