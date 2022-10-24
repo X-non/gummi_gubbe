d4e83eaf2c728594fa33b844bd7f6494bf5520d0
Casper <casper.bjorkman@gmail.com>
2022-10-24 10:27:52 +0200

    formating

--- pp/tree_cleaner.py
+++ pp/tree_cleaner.py
@@ -1,13 +1,31 @@
-
-
 from PP import *
 
-test = {"kind":"add",
-"left":{"expr":{"kind":"mul",
-                "left":{"kind":"num", "val":"1"}, "right":{"expr":{"kind":"add",
-                "left":{"kind":"num", "val":"1"}, "right":{"kind":"num", "val":"1"}}, "kind":"par"}},"kind":"par"},
-"right":{"expr":{"kind":"add",
-                "left":{"kind":"num", "val":"1"}, "right":{"kind":"num", "val":"1"}},"kind":"par"}}
+test = {
+    "kind": "add",
+    "left": {
+        "expr": {
+            "kind": "mul",
+            "left": {"kind": "num", "val": "1"},
+            "right": {
+                "expr": {
+                    "kind": "add",
+                    "left": {"kind": "num", "val": "1"},
+                    "right": {"kind": "num", "val": "1"},
+                },
+                "kind": "par",
+            },
+        },
+        "kind": "par",
+    },
+    "right": {
+        "expr": {
+            "kind": "add",
+            "left": {"kind": "num", "val": "1"},
+            "right": {"kind": "num", "val": "1"},
+        },
+        "kind": "par",
+    },
+}
 
 
 """Onödiga paranteser:
@@ -18,27 +36,19 @@ När det endast finns add och sub kvar i den "grenen" av trädet ex:
  """
 
 
-
-
-
-Symbols = {
-    "add",
-    "sub",
-    "mul",
-    "div",
-    "par"
-}
+Symbols = {"add", "sub", "mul", "div", "par"}
 
 non_remove_binary_nodes = ["mul", "div"]
 remove_binary_nodes = ["add", "sub"]
 unary_nodes = ["par", "u-sub"]
 literal_nodes = ["var", "num"]
 
+
 def tree_cleaner(node):
-    kind=node["kind"]
+    kind = node["kind"]
 
     if kind in literal_nodes:
-       return node
+        return node
 
     if kind == "par":
         return tree_cleaner(node["expr"])
@@ -51,12 +61,12 @@ def tree_cleaner(node):
             left = tree_cleaner(left["expr"])
         if right["kind"] == "par":
             right = tree_cleaner(right["expr"])
-        return {"kind":kind, "left":left, "right":right}
+        return {"kind": kind, "left": left, "right": right}
 
     if kind in non_remove_binary_nodes:
         left = node["left"]
         right = node["right"]
-        return {"kind":kind, "left":left, "right":right}
+        return {"kind": kind, "left": left, "right": right}
 
 
 print(format_node(tree_cleaner(test)))
