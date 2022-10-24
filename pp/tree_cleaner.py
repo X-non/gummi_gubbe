7bfc8fd0159ce53b69785e767d30b4c084212db7
Casper <casper.bjorkman@gmail.com>
2022-10-24 11:24:04 +0200

    refactored out binary cleaning

--- pp/tree_cleaner.py
+++ pp/tree_cleaner.py
@@ -27,6 +27,8 @@ När det endast finns add och sub kvar i den "grenen" av trädet ex:
 
 Symbols = {"add", "sub", "mul", "div", "par"}
 
+binary_nodes = ["mul", "sub", "div", "add"]
+
 non_remove_binary_nodes = ["mul"]
 remove_binary_nodes = ["add", "div"]
 unary_nodes = ["par", "u-sub"]
@@ -43,37 +45,35 @@ def remove_paren(node):
 def tree_cleaner(node):
     kind = node["kind"]
 
+    if kind in binary_nodes:
+        return clean_binary(node)
+
+    if kind in unary_nodes:
+        #  u-sub containsa implicit parens if the containing expr
+        # is not a literal thus we can remove the directly containing parens
+        expr = remove_paren(node["expr"])
+        return {"kind": kind, "expr": tree_cleaner(expr)}
+
+    return node
+
+
+def clean_binary(node):
+    kind = node["kind"]
+
+    left = node["left"]
+    right = node["right"]
+
     if kind in remove_binary_nodes:
-        left = node["left"]
-        right = node["right"]
-        if left["kind"] == "par":
-            left = remove_paren(left)
-        if right["kind"] == "par":
-            right = remove_paren(right)
+        left = remove_paren(left)
+        right = remove_paren(right)
 
-        return {"kind": kind, "left": tree_cleaner(left), "right": tree_cleaner(right)}
-
-    if kind == "sub":
-        left = node["left"]
-        right = node["right"]
-        if left["kind"] == "par":
-            left = remove_paren(left)
+    elif kind == "sub":
+        left = remove_paren(left)
 
         if right["kind"] == "par" and right["expr"]["kind"] in literal_nodes:
             right = remove_paren(right)
 
-        return {"kind": kind, "left": tree_cleaner(left), "right": tree_cleaner(right)}
-
-    if kind in non_remove_binary_nodes:
-        left = node["left"]
-        right = node["right"]
-        return {"kind": kind, "left": tree_cleaner(left), "right": tree_cleaner(right)}
-
-    if kind in unary_nodes:
-        expr = tree_cleaner(node["expr"])
-        return {"kind": kind, "expr": expr}
-
-    return node
+    return {"kind": kind, "left": tree_cleaner(left), "right": tree_cleaner(right)}
 
 
 print(format_node(test))
